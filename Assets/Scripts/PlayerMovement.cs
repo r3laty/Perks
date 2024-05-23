ff16288cba77d28495bea9d91a41f45c6a92b7f2
r3laty <lolkers.ilya@gmail.com>
2024-05-23 13:47:48 +0300

    Add character controller

---
+++ Assets/Scripts/PlayerMovement.cs
@@ -0,0 +1,36 @@
+using UnityEngine;
+[RequireComponent(typeof(Rigidbody))]
+public class PlayerMovement : MonoBehaviour
+{
+    [SerializeField] private float speed = 6f;
+
+    private Rigidbody _rb;
+    private Vector3 _move;
+    private void Awake()
+    {
+        _rb = GetComponent<Rigidbody>();
+    }
+    void Update()
+    {
+        float x = Input.GetAxisRaw("Horizontal");
+        float z = Input.GetAxisRaw("Vertical");
+
+        _move = new Vector3(x, 0, z);
+
+        Quaternion rot = transform.rotation;
+
+        Quaternion newRot = Quaternion.LookRotation(Vector3.Normalize(_move));
+
+        transform.rotation = Quaternion.Lerp(rot, newRot, Time.deltaTime * 10);
+
+    }
+    private void FixedUpdate()
+    {
+        Move();
+    }
+    private void Move()
+    {
+        Vector3 move = new Vector3(_move.x * speed * Time.fixedDeltaTime, _rb.velocity.y, _move.z * speed * Time.fixedDeltaTime);
+        _rb.velocity = move;
+    }
+}
